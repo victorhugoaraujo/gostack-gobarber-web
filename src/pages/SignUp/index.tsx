a99ef402e74136a9b9a0fecf9460f2407ea25c74
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-04-22 21:55:35 -0300

    SignIn and SignUp pages styling, Add unform

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="Gobarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faca seu Cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="/">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
